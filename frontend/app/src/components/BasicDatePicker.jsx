4f0bc376397e5135ff90466836a661a3b7890910
David Johns <djjohns42@gmail.com>
2023-08-16 22:05:23 -0500

    Add a basic date picker component.

---
+++ frontend/app/src/components/BasicDatePicker.jsx
@@ -0,0 +1,14 @@
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+export default function BasicDatePicker() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DatePicker']}>
+        <DatePicker label="Basic date picker" />
+      </DemoContainer>
+    </LocalizationProvider>
+  );
+}
